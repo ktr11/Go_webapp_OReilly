2207fd6652b89d87aebae761462600186ddc92d8
ktr11 <kenmcz315@gmail.com>
2019-09-15 12:35:59 +0900

    p.17 まで ただし送信ボタンを押すとWebSocketが切断されるという問題がある

--- chapter01/chat/client.go
+++ chapter01/chat/client.go
@@ -4,6 +4,30 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// read
+// クライアントがWebSocketからReadMessageを使ってデータを読み込むために使われます。
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+		c.socket.Close()
+	}
+}
+
+// write
+// 継続的にsendチャネルからメッセージを受け取り、WebSocketのWriteMessageメソッドを使ってこれを書き出します。
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
 // client はチャットを行っている1人のユーザーを表す
 type client struct {
 	// socketはこのクライアントのためのwebsocketです。
